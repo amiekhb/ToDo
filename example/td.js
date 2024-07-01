b0cf276ab0937ff75c12d7df135bd11038560d17
amiekhb <aminaaami0715@gmail.com>
2024-07-01 12:42:32 +0800

    first

---
+++ example/td.js
@@ -0,0 +1,84 @@
+// DOM ELEMEMTS
+const taskTodoList = document.getElementById("taskTodoList");
+const taskProgressList = document.getElementById("taskProgressList");
+const taskDoneList = document.getElementById("taskDoneList");
+const taskBlockedList = document.getElementById("taskBlockedList");
+const addTaskBtn = document.getElementById("addTaskBtn");
+
+// VARIABLES FOR TASK
+const tasks = [
+  {
+    name: "Task One",
+    status: "TODO",
+  },
+  {
+    name: "Task Two",
+    status: "INPROGRESS",
+  },
+  {
+    name: "Task Three",
+    status: "BLOCKED",
+  },
+];
+
+function draw() {
+  taskTodoList.innerHTML = "";
+  taskProgressList.innerHTML = "";
+  taskDoneList.innerHTML = "";
+  taskBlockedList.innerHTML = "";
+
+  for (let i = 0; i < tasks.length; i++) {
+    console.log("TASKS", tasks);
+    const newTaskCard = `
+    <div class="d-flex justify-content-between align-items-center border border-1 rounded p-2">
+    <span>${tasks[i].name}</span>
+    <div>
+        <button class="btn">
+        <i class="bi bi-pencil"></i>
+        </button>
+        <button class="btn">
+        <i class="bi bi-trash"></i>
+        </button>
+    </div>
+    </div>
+ `;
+
+    switch (tasks[i].status) {
+      case "TODO": {
+        taskTodoList.innerHTML += newTaskCard;
+        break;
+      }
+      case "INPROGRESS": {
+        taskProgressList.innerHTML += newTaskCard;
+        break;
+      }
+      case "DONE": {
+        taskDoneList.innerHTML += newTaskCard;
+        break;
+      }
+      case "BLOCKED": {
+        taskBlockedList.innerHTML += newTaskCard;
+        break;
+      }
+      default: {
+        console.log("ALDAA GARLAA");
+      }
+    }
+  }
+}
+
+addTaskBtn.addEventListener("click", function () {
+  tasks[1].status = "DONE";
+  draw();
+  console.log("TASKS", tasks);
+});
+draw();
+
+addTaskBtn.addEventListener("click", () => {
+  const newTask = {
+    name: "shine",
+    status: "TODO",
+  };
+  tasks.push;
+  zurah();
+});
