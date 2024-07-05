74f4bcf016f81d469c22dfa909cd5511b4961f72
amiekhb <aminaaami0715@gmail.com>
2024-07-05 11:46:24 +0800

    a

--- todo.js
+++ todo.js
@@ -7,6 +7,7 @@ const addTaskBtn = document.getElementById("addTaskBtn");
 const saveBtn = document.getElementById("save-btn");
 const taskInput = document.getElementById("task-input");
 const taskStatus = document.getElementById("status");
+const totalTasks = document.getElementsByClassName("totalTasks");
 
 // VARIABLES FOR TASK
 let isEdited = false;
@@ -31,17 +32,17 @@ function zurah() {
   taskProgressList.innerHTML = "";
   taskDoneList.innerHTML = "";
   taskBlockedList.innerHTML = "";
-
+  totalTasks.innerHTML = "";
   for (let i = 0; i < tasks.length; i++) {
     console.log("TASKS", tasks);
     const newTaskCard = `
     <div class="d-flex justify-content-between align-items-center border border-1 rounded p-2">
         <span>${tasks[i].name} - ${i}</span>
         <div>
-            <button class="btn"   data-bs-toggle="modal" data-bs-target="#taskModal" onclick="editTask(${i})">    
+            <button style="color: #e8ecef" class="btn"   data-bs-toggle="modal" data-bs-target="#taskModal" onclick="editTask(${i})">    
             <i class="bi bi-pencil "></i>
             </button>
-            <button class="btn" onclick="deleteTask(${i})">
+            <button style="color: red" class="btn" onclick="deleteTask(${i})">
                 <i class="bi bi-trash"></i>
             </button>
         </div>
@@ -51,18 +52,22 @@ function zurah() {
     switch (tasks[i].status) {
       case "TODO": {
         taskTodoList.innerHTML += newTaskCard;
+
         break;
       }
       case "INPROGRESS": {
         taskProgressList.innerHTML += newTaskCard;
+
         break;
       }
       case "DONE": {
         taskDoneList.innerHTML += newTaskCard;
+
         break;
       }
       case "BLOCKED": {
         taskBlockedList.innerHTML += newTaskCard;
+
         break;
       }
       default: {
@@ -114,12 +119,12 @@ const editTask = (taskIndex) => {
   editedIndex = taskIndex;
 };
 
-// function taskAmount() {
-//   const totalTasks = document.getElementsByClassName("totalTasks");
-//   totalTasks.innerTEXT = tasks.length;
+function taskAmount() {
+  const totalTasks = document.getElementsByClassName("totalTasks");
+  totalTasks.innerTEXT = tasks.length;
 
-//   return totalTasks;
-// }
-// const listItems = document.querySelectorAll(".totalTasks");
-// const count = listItems.length;
-// console.log(count);
+  return totalTasks;
+}
+const listItems = document.querySelectorAll(".totalTasks");
+const count = listItems.length;
+console.log(count);
